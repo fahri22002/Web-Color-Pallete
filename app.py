ed7398cb2b97fb051a4b82d2bd161c0320ceb1f9
unknown <fahrinizar23@gmail.com>
2024-06-10 13:30:07 +0700

    a

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import streamlit as st
+
 from flask import Flask, render_template, request, redirect, url_for
 import cv2
 import numpy as np
@@ -59,4 +59,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
